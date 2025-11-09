1fc4f08f8762c603e9c9ac7251817b499f5a20c7
a-sriel <rilescentauri@gmail.com>
2025-11-09 11:45:06 -0600

    Fixed popup logic

--- bait-and-switch/Scenes/Game/popup_window.gd
+++ bait-and-switch/Scenes/Game/popup_window.gd
@@ -4,10 +4,13 @@ var is_bait = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$"../../Hook".fish_is_caught.connect(create_new_message)
 	pass # Replace with function body.
 
 func create_new_message() -> void:
-	var email = self.get_child(randi() % 6)
+	var email = self.get_child(randi() % 5)
+	self.visible = true
+	email.visible = true
 	if email.name.contains("False"):
 		is_bait = true
 	else:

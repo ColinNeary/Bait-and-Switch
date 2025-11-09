1b0aa1f973ebdd99b6445b14f318f109553dd10a
a-sriel <rilescentauri@gmail.com>
2025-11-09 11:31:58 -0600

    Added boolean logic to main

--- bait-and-switch/Scenes/Game/game.gd
+++ bait-and-switch/Scenes/Game/game.gd
@@ -8,3 +8,12 @@ var current_money:int = Globals.starting_money
 
 func _ready() -> void:
 	money_label.text = "$" + str(current_money)
+
+func correct_answer_gotten() -> void:
+	current_money += 20
+
+func wrong_answer_gotten() -> void:
+	current_money -= 50
+
+	if current_money <= 0:
+		get_tree().change_scene_to_file("res://UI/Main Menu.tscn")
--- bait-and-switch/Scenes/Game/popup_window.gd
+++ bait-and-switch/Scenes/Game/popup_window.gd
@@ -1,25 +1,32 @@
 extends Panel
 
-var is_bait = true
+var is_bait = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func create_new_message() -> void:
+	var email = self.get_child(randi() % 6)
+	if email.name.contains("False"):
+		is_bait = true
+	else:
+		is_bait = false
 	pass
-
 
 func _on_bait_button_pressed() -> void:
 	self.visible = false
-	# if is_bait:
-		
+	if is_bait:
+		self.get_parent().get_parent().correct_answer_gotten()
+	else:
+		self.get_parent().get_parent().wrong_answer_gotten()
 	pass # Replace with function body.
 
 
 func _on_reel_button_pressed() -> void:
 	self.visible = false
-	# if !is_bait:
+	if !is_bait:
+		self.get_parent().get_parent().correct_answer_gotten()
+	else:
+		self.get_parent().get_parent().wrong_answer_gotten()
 	pass # Replace with function body.
